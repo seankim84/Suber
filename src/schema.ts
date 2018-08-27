4aeca0bd81252cca0c1581d3cd7942b9672edf06
SeanKim <sean.kim84@gmail.com>
2018-08-27 11:40:15 +0700

    API and Schema structure Part 1

---
+++ src/schema.ts
@@ -0,0 +1,5 @@
+
+import { GraphQLSchema } from 'graphql';
+import { makeExecutableSchema } from 'graphql-tools';
+import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
+import path from 'path';
