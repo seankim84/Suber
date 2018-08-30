74e47c4558139c79aa17528d26bdceb7ec19c90f
SeanKim <sean.kim84@gmail.com>
2018-08-30 10:33:10 +0700

    Planing the Resolvers Part 1

--- src/types/resolvers.d.ts
+++ src/types/resolvers.d.ts
@@ -3,5 +3,7 @@ export type Resolver = (parent: any, args: any, context: any, info: any) => any;
 export interface Resolvers {
     [key: string]: {
         [key: string]: Resolver
-    }
+    };
 }
+
+
