502795c6cc5988efa8136259929f1fbbcdbce87b
SeanKim <sean.kim84@gmail.com>
2018-11-21 12:49:31 +0700

    Updating Profile Resolvers

---
+++ src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
@@ -0,0 +1,38 @@
+import privateResolver from "../../../utils/privateResolver";
+import User from "../../../entities/User";
+import { Resolvers } from "../../../types/resolvers";
+import { UpdateMyProfileMutationArgs, UpdateMyProfileResponse } from '../../../types/graph';
+
+const resolvers: Resolvers = {
+    Mutation: {
+        UpdateMyProfile: privateResolver(
+        async(
+            _, 
+            args: UpdateMyProfileMutationArgs, 
+            { req }
+            ): Promise<UpdateMyProfileResponse> => {
+                const user: User = req.user;
+                const notNull:any = cleanNullArgs(args); // 👈🏻 Add ':any'
+                    if(notNull.password !== null) { // 👈🏻 Change from args to notNull
+                        user.password = notNull.password; // 👈🏻 Same here
+                        user.save();
+                        delete notNull.password; 
+                    }
+                    try {
+                        await User.update({ id: user.id }, { ...notNull }); 
+                        return {
+                            ok: true,
+                            error: null
+                        };
+                    } catch (error) {
+                        return {
+                            ok: false,
+                            error: error.message
+                        };
+                    }
+                }
+            )
+        }
+    }
+
+export default resolvers;
