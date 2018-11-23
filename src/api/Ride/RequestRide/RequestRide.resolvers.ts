1c6ecf242c2ca3ffa378dd19b391b2693779e19d
SeanKim <sean.kim84@gmail.com>
2018-11-23 11:39:50 +0700

    UpdatingRideStatus Resolvers

--- src/api/Ride/RequestRide/RequestRide.resolvers.ts
+++ src/api/Ride/RequestRide/RequestRide.resolvers.ts
@@ -1,30 +1,49 @@
-
-import privateResolver from "src/utils/privateResolver";
-import { RequestRideMutationArgs, RequestRideResponse } from '../../../types/graph';
-import { Resolvers } from "src/types/resolvers";
-import Ride from '../../../entities/Ride';
+import Ride from "../../../entities/Ride";
 import User from "../../../entities/User";
+import {
+    RequestRideMutationArgs,
+    RequestRideResponse
+} from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
 
 const resolvers: Resolvers = {
-  Mutation: {
-    RequestRide: privateResolver( async (_, args: RequestRideMutationArgs, { req }): Promise<RequestRideResponse> => {
-        const user: User = req.user;
-        try {
-            const ride = await Ride.create({ ...args, passenger: user }).save();
-            return {
-                ok: true,
-                error: null,
-                ride
-            };
-        } catch(error){
-            return {
-                ok: false,
-                error: error.message,
-                ride: null
-            };
-        }
-      }
-    )}
+    Mutation: {
+        RequestRide: privateResolver(
+            async (
+                _,
+                args: RequestRideMutationArgs,
+                { req, pubSub }
+            ): Promise<RequestRideResponse> => {
+                const user: User = req.user;
+                if (!user.isRiding) {
+                    try {
+                        const ride = await Ride.create({ ...args, passenger: user }).save();
+                        pubSub.publish("rideRequest", { NearbyRideSubscription: ride });
+                        user.isRiding = true;
+                        user.save();
+                        return {
+                            ok: true,
+                            error: null,
+                            ride
+                        };
+                    } catch (error) {
+                        return {
+                            ok: false,
+                            error: error.message,
+                            ride: null
+                        };
+                    }
+                } else {
+                    return {
+                        ok: false,
+                        error: "You can't request two rides",
+                        ride: null
+                    };
+                }
+            }
+        )
+    }
 };
 
 export default resolvers;
